67d97eb1479313d2eefd94b9c5da3ad3f09ec0d4
Robert Wigginton <rswigginton@gmail.com>
2026-03-06 14:32:43 -0700

    podman

--- hosts/common/podman.nix
+++ hosts/common/podman.nix
@@ -1,13 +1,18 @@
 { pkgs, ... }: {
-  environment.systemPackages = [
-    pkgs.distrobox
-    pkgs.podman-compose
+  environment.systemPackages = with pkgs; [
+    distrobox
+    lazydocker
+    docker-compose
+    podman-compose
+    (writeShellScriptBin "docker-compose" ''exec docker compose "$@"'')
   ];
+
   virtualisation = {
     containers.enable = true;
     podman = {
       enable = true;
       dockerCompat = true;
+      dockerSocket.enable = true;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
