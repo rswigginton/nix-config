9e8cdaa61a184c8894a8aee5f694620066f1e461
Robert Wigginton <rswigginton@gmail.com>
2026-03-21 16:33:50 -0600

    update podman

--- hosts/common/podman.nix
+++ hosts/common/podman.nix
@@ -6,6 +6,8 @@
     (writeShellScriptBin "docker-compose" ''exec podman-compose "$@"'')
   ];
 
+  environment.variables.DOCKER_HOST = "unix:///run/user/1000/podman/podman.sock";
+
   virtualisation = {
     containers.enable = true;
     podman = {
