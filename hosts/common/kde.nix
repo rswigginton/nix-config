e0baa1e0d20f0488a7e3aee5cd53f92cffda6c45
Robert Wigginton <rswigginton@gmail.com>
2026-04-25 10:08:34 -0600

    update kde

--- hosts/common/kde.nix
+++ hosts/common/kde.nix
@@ -1,3 +1,17 @@
 { pkgs, ... }: {
   services.desktopManager.plasma6.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
+    config.kde.default = [ "kde" ];
+  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    wireplumber.enable = true;
+  };
 }
